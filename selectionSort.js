930830158d4c87ba4637a982b8d1c63aab9fa9a7
MayuriR29 <mayuri.gr@gmail.com>
2018-07-30 22:37:49 +0800

    added solution to kata

---
+++ selectionSort.js
@@ -0,0 +1,22 @@
+const swap = (arr, index1, index2) => {
+  const arrCopy = arr.slice();
+  [arrCopy[index1], arrCopy[index2]] = [arrCopy[index2], arrCopy[index1]];
+  return arrCopy;
+};
+
+const getMinIndex = slicedArr => {
+  const min = Math.min(...slicedArr);
+  return slicedArr.indexOf(min);
+};
+
+const selectionSort = array => {
+  let minIndex;
+  let sortedArray = [];
+  let swappedArray = [];
+  if (array.length === 1 || array.length === 0) return array;
+  minIndex = getMinIndex(array); //[ 7, 2, 8, 10 ]//2,8,10
+  swappedArray = swap(array, 0, minIndex);
+  sortedArray.push(swappedArray[0]);
+  return sortedArray.concat(selectionSort(swappedArray.slice(1)));
+};
+module.exports = selectionSort;
---
+++ selectionSort.test.js
@@ -0,0 +1,13 @@
+const selectionSort = require("./selectionSort");
+
+test("test when array length is 1,return array", () => {
+  expect(selectionSort([1])).toEqual([1]);
+});
+test("test should return sorted array", () => {
+  let arr1 = [2, 1];
+  let arr2 = [2, 5, 3];
+  let arr3 = [4, 7, 2, 8, 10];
+  expect(selectionSort(arr1)).toEqual([1, 2]);
+  expect(selectionSort(arr2)).toEqual([2, 3, 5]);
+  expect(selectionSort(arr3)).toEqual([2, 4, 7, 8, 10]);
+});
